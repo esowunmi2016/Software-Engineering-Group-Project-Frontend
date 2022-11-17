468440a2b320f127e6dc6d2e55d917ebb4c8a5f6
esowunmi2016 <esowunmi2016@fau.edu>
2022-11-17 12:42:58 -0500

    .

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes >
           <Route path='/' element={<Signin />} />
           <Route path='/signin' element={<Signin />} />
